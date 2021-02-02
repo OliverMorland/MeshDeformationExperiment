f52e6003eb9b6d91932d19b864a2e61607d64b16
Oliver Morland <oliversimonedward@hotmail.co.uk>
2021-02-02 07:31:25 -0500

    Expanded grid and placed all grid points in single GameObject

--- Assets/Scripts/Deformable.cs
+++ Assets/Scripts/Deformable.cs
@@ -7,11 +7,13 @@ public class Deformable : MonoBehaviour
     MeshFilter m_meshFilter;
     Vector3[] m_uvw;
     public GameObject m_BezierSurfacePointPrefab;
+    public GameObject[] m_ControlPoints;
     public GameObject[] m_ControlPointsTop;
     public GameObject[] m_ControlPointsBottom;
     GameObject[] SurfacePoints;
-    public int n = 2;
-    public int m = 2;
+    public int n = 3;
+    public int m = 3;
+    public int l = 1;
     float resolution = 10f;
   
     Vector3[] m_vertices;
@@ -19,6 +21,7 @@ public class Deformable : MonoBehaviour
 
     public float m_speed = 10f;
     public float m_maxAmplitude = 1f;
+    public int m_layerIndex = 0;
     public bool m_flipNormals = false;
 
 
@@ -31,21 +34,21 @@ public class Deformable : MonoBehaviour
 
         m_uvw = new Vector3[vertices.Length];
 
-
-        Vector3 min = m_ControlPointsTop[0].transform.position;
-        Vector3 max = m_ControlPointsTop[0].transform.position;
-        for (int i = 0; i < m_ControlPointsTop.Length; i++)
+        Vector3 min = m_ControlPoints[0].transform.position;
+        Vector3 max = m_ControlPoints[0].transform.position;
+        for (int i = 0; i < m_ControlPoints.Length; i++)
         {
-            min.x = Mathf.Min(min.x, m_ControlPointsTop[i].transform.position.x);
-            min.y = Mathf.Min(min.y, m_ControlPointsTop[i].transform.position.y);
-            min.z = Mathf.Min(min.z, m_ControlPointsTop[i].transform.position.z);
+            min.x = Mathf.Min(min.x, m_ControlPoints[i].transform.position.x);
+            min.y = Mathf.Min(min.y, m_ControlPoints[i].transform.position.y);
+            min.z = Mathf.Min(min.z, m_ControlPoints[i].transform.position.z);
 
-            max.x = Mathf.Max(max.x, m_ControlPointsTop[i].transform.position.x);
-            max.y = Mathf.Max(max.y, m_ControlPointsTop[i].transform.position.y);
-            max.z = Mathf.Max(max.z, m_ControlPointsTop[i].transform.position.z);
+            max.x = Mathf.Max(max.x, m_ControlPoints[i].transform.position.x);
+            max.y = Mathf.Max(max.y, m_ControlPoints[i].transform.position.y);
+            max.z = Mathf.Max(max.z, m_ControlPoints[i].transform.position.z);
 
         }
 
+        /*
         for (int i = 0; i < m_ControlPointsBottom.Length; i++)
         {
             min.x = Mathf.Min(min.x, m_ControlPointsBottom[i].transform.position.x);
@@ -55,8 +58,8 @@ public class Deformable : MonoBehaviour
             max.x = Mathf.Max(max.x, m_ControlPointsBottom[i].transform.position.x);
             max.y = Mathf.Max(max.y, m_ControlPointsBottom[i].transform.position.y);
             max.z = Mathf.Max(max.z, m_ControlPointsBottom[i].transform.position.z);
-
         }
+        */
 
         Vector3 delta = max - min;
 
@@ -108,8 +111,6 @@ public class Deformable : MonoBehaviour
 
 
 
-
-
     void UpdateVertexPositions()
     {
         //Populating vertice array
@@ -122,26 +123,34 @@ public class Deformable : MonoBehaviour
             float u = m_uvw[i].z;
             float v = m_uvw[i].x;
             float w = m_uvw[i].y;
-            //Vector3 pos = m_uvw[i];
-            Vector3 topPos = GetBezierSurfacePosition(v, u, n, m, m_ControlPointsTop);
-            Vector3 bottomPos = GetBezierSurfacePosition(v, u, n, m, m_ControlPointsBottom);
 
+
+            //Determine Control Points to use
+            int layerIndex = 0;
+            float layer_w = 0;
+            while (layer_w < w)
+            {
+                layer_w += 1 / l;
+                layerIndex++;
+            }
+
+            GameObject[] controlPointsTopLayer = m_ControlPointsTop;
+            GameObject[] controlPointsBottonLayer = m_ControlPointsBottom;
+
+
+            //Find positions over top and bottom layers of grid
+            Vector3 topPos = GetBezierSurfacePosition(v, u, n, m, controlPointsTopLayer);
+            Vector3 bottomPos = GetBezierSurfacePosition(v, u, n, m, controlPointsBottonLayer);
+
+            //Interpolate position using w and top and bottom points
             Vector3 pos = Vector3.Lerp(bottomPos, topPos, w);
-
             m_vertices[i] = transform.InverseTransformPoint(pos);
 
 
         }
 
         //Add vertices to mesh
-        for (int i = 0; i < m_uvw.Length; i++)
-        {
-            Debug.Log($"vertices: {m_vertices[i]} ||| u: {m_uvw[i]}");
-        }
-
         m_meshFilter.mesh.vertices = m_vertices;
-
-
         m_meshFilter.mesh.RecalculateNormals();
         //Add mesh to mesh filter
         //m_meshFilter = GetComponent<MeshFilter>();
@@ -149,7 +158,16 @@ public class Deformable : MonoBehaviour
     }
 
 
+    GameObject [] GetControlPointsOfLayer(int layerIndex)
+    {
+        GameObject[] layerControlPoints = new GameObject[(n + 1) * (m + 1)];
+        for (int i = 0; i < layerControlPoints.Length; i++)
+        {
+            layerControlPoints[i] = m_ControlPoints[i + layerIndex * (n + 1) * (m + 1)];
+        }
 
+        return layerControlPoints;
+    }
 
 
     GameObject FindPoint(int c, int r, GameObject [] controlPoints)
@@ -198,7 +216,6 @@ public class Deformable : MonoBehaviour
         Vector3 SUM_V = new Vector3(0, 0, 0);
         for (int c = 0; c <= n; c++)
         {
-
             Vector3 SUM_U = new Vector3(0, 0, 0);
             for (int r = 0; r <= m; r++)
             {
